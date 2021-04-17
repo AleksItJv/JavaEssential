3cc3d8785267870bd9dc81fd2ce3f6559b317039
AleksItJv <aleks.it.jv@gmail.com>
2021-04-17 17:59:12 +0300

    MASTER-5: tenth homework - essential

--- src/homework10/hexToDecimal/HexToDecimal.java
+++ src/homework10/hexToDecimal/HexToDecimal.java
@@ -3,13 +3,25 @@ package homework10.hexToDecimal;
 public class HexToDecimal {
 
     public static void main(String[] args) {
-        String number = "B5ae452";
+        String number = "-C";
         System.out.println(new HexToDecimal().hexToDecimal(number));
     }
 
     public int hexToDecimal(String number) {
         char ch[] = number.toCharArray();
         int result = 0;
+        if (ch[0] == '-'){
+            char newCh[] = new char[ch.length - 1];
+            for (int i = 1; i < ch.length; i++){
+                newCh[i - 1] = ch[i];
+
+            }
+            ch = newCh;
+            for (int i = ch.length - 1; i >= 0; i--) {
+                result += Character.getNumericValue(ch[i]) * (int) Math.pow(16, (ch.length - 1 - i));
+            }
+            return - result;
+        }
         for (int i = ch.length - 1; i >= 0; i--) {
             result += Character.getNumericValue(ch[i]) * (int) Math.pow(16, (ch.length - 1 - i));
         }
